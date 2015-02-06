1a7e9152bffcb9b3a36e34a12171d65a697e71a6
christian otkjær <christian.otkjaer@gmail.com>
2015-02-06 11:27:36 +0100

    Initial Menu Button

---
+++ Pod/Classes/SIMenuButton.h
@@ -0,0 +1,13 @@
+//
+//  SIMenuButton.h
+//  Pods
+//
+//  Created by Christian Otkjær on 06/02/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIMenuButton : UIControl
+
+@end
---
+++ Pod/Classes/SIMenuButton.m
@@ -0,0 +1,13 @@
+//
+//  SIMenuButton.m
+//  Pods
+//
+//  Created by Christian Otkjær on 06/02/15.
+//
+//
+
+#import "SIMenuButton.h"
+
+@implementation SIMenuButton
+
+@end
